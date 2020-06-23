76c88b9b14d84501b5373ebea1a12d25b58fbc38
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-06-23 15:06:08 +0700

    Change to send file url to client

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 $filepath = "ENTER YOUR FILE PATH HERE";
+$fileurl = "ENTER YOUR FILE URL HERE";
 $client_md5 = $_GET['md5'];
 $result = array('updated' => true);
 if (md5_file($filepath) !== $client_md5)
 {
     $result['updated'] = false;
-    $result['filepath'] = $filepath;
+    $result['fileurl'] = $fileurl;
 }
 echo json_encode($result);
 ?>
