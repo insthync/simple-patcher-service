214cfc2a33f5b134dd8a7ae614585c9563ca6241
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-06-23 15:32:45 +0700

    Send file md5 to client

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 $filepath = "ENTER YOUR FILE PATH HERE";
 $fileurl = "ENTER YOUR FILE URL HERE";
+$filemd5 = md5_file($filepath);
 $client_md5 = $_GET['md5'];
 $result = array('updated' => true);
-if (md5_file($filepath) !== $client_md5)
+if ($filemd5 !== $client_md5)
 {
     $result['updated'] = false;
     $result['fileurl'] = $fileurl;
+    $result['filemd5'] = $filemd5;
 }
 echo json_encode($result);
 ?>
