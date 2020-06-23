1dc8889928e44038f3b1c24e5d27731b03775b5c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-06-23 22:09:02 +0700

    Update demo content

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
-$filepath = "ENTER YOUR FILE PATH HERE";
-$fileurl = "ENTER YOUR FILE URL HERE";
+$filepath = "./file.zip";
+$fileurl = "http://localhost/ve_patcher/file.zip";
 $filemd5 = md5_file($filepath);
+$windowsexe = "VECY.exe";
+$osxexe = "VECY.app";
 $notice = "Hello\nWelcome everybody :)";
 $client_md5 = $_GET['md5'];
-$result = array('updated' => true, 'notice' => $notice);
+$result = array(
+    'updated' => true,
+    'notice' => $notice,
+    'windowsexe' => $windowsexe,
+    'osxexe' => $osxexe);
 if ($filemd5 !== $client_md5)
 {
     $result['updated'] = false;
