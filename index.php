c085075ac2bbd8c044832efb0718bb790340bd34
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-06-23 21:03:31 +0700

    Add notice

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 $filepath = "ENTER YOUR FILE PATH HERE";
 $fileurl = "ENTER YOUR FILE URL HERE";
 $filemd5 = md5_file($filepath);
+$notice = "Hello\nWelcome everybody :)";
 $client_md5 = $_GET['md5'];
-$result = array('updated' => true);
+$result = array('updated' => true, 'notice' => $notice);
 if ($filemd5 !== $client_md5)
 {
     $result['updated'] = false;
